15d3405b419603233361fbd65fc02d70e93602f6
Abdul Ghaffar <ghaffarbajwa9@gmail.com>
2023-12-07 10:28:14 +0500

    name and usertype fields are added in user model

--- app/admin/users.rb
+++ app/admin/users.rb
@@ -1,10 +1,12 @@
 ActiveAdmin.register User do
-  permit_params :email, :password, :password_confirmation
+  permit_params :name, :email, :password, :password_confirmation, :usertype
 
   index do
     selectable_column
     id_column
+    column :name
     column :email
+    column :usertype
     column :current_sign_in_at
     column :sign_in_count
     column :created_at
@@ -18,9 +20,11 @@ ActiveAdmin.register User do
 
   form do |f|
     f.inputs do
+      f.input :name
       f.input :email
       f.input :password
       f.input :password_confirmation
+      f.input :usertype, :as => :select, collection: (['manager', 'developer', 'qa'])
     end
     f.actions
   end
