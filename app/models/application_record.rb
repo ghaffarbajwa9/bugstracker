0af05c9ef5bf8dc1d5ef765e5f144c44718ce644
Abdul Ghaffar <ghaffarbajwa9@gmail.com>
2023-12-07 10:15:57 +0500

    initialized

---
+++ app/models/application_record.rb
@@ -0,0 +1,6 @@
+class ApplicationRecord < ActiveRecord::Base
+  primary_abstract_class
+  def self.ransackable_attributes(auth_object = nil)
+    ["created_at", "email", "encrypted_password", "id", "id_value", "remember_created_at", "reset_password_sent_at", "reset_password_token", "updated_at"]
+  end
+end
