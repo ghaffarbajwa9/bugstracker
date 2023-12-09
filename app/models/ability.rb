d74fb286dcae0fad92013201adc501d26e41bff3
Abdul Ghaffar <ghaffarbajwa9@gmail.com>
2023-12-09 23:59:28 +0500

    developer can only see the projects he is part of

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -18,13 +18,14 @@ class Ability
       can :manage, Bug
     end 
     if user.developer?
-      can :read, Project
+      can :read, Project, users:{id: user.id}
+      can :read, Bug
       can :update, Bug
     end
     if user.qa?
       can :read, Project
-      can :create, Bug
       can :read, Bug
+      can :create, Bug
     end 
   end
 end
